36a8d787db3392ff6850d3a938c75996a018e2d2
Eduardo Olivares <e.olivares.abcd@gmail.com>
2025-04-10 15:48:11 +0200

    employee class filled

---
+++ unit_testing/test_employee.py
@@ -0,0 +1,6 @@
+import unittest
+from employee import Employee
+
+# TODO fill all this with tests!
+# README this is a message to read, searchable through the TODOTREE extension
+# FIXME this is a fixme locatable tag
