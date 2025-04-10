3568ed660f03f698e1869d73435323a47bff2940
Eduardo Olivares <e.olivares.abcd@gmail.com>
2025-04-10 15:22:32 +0200

    added subtract multyply and divide functions

--- unit_testing/test_calc.py
+++ unit_testing/test_calc.py
@@ -3,8 +3,13 @@ import calc
 
 class TestCalc(unittest.TestCase): # pick any name you want for this class. it inherits from the unittest.TestCase class.
     def test_add(self): # all methods from the TestCalc class must start with "test_" so that they run properly
-        result = calc.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1,-1), -2) # added some boundary / problematic conditions
+        self.assertEqual(calc.add(-1, 1), 0) # even if we have many methods, all this still counts as a single test.
+
+
 
 if __name__ == '__main__':
     unittest.main()
+
+# when running the test script, we get a dot for each successful test and an F for each failed test
