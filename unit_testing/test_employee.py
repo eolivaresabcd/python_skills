231d21a49e5512318f0985cd07b14c2c84364172
Eduardo Olivares <e.olivares.abcd@gmail.com>
2025-04-10 15:57:46 +0200

    set different TODO like lables to see how thwe TODO extension works

--- unit_testing/test_employee.py
+++ unit_testing/test_employee.py
@@ -4,3 +4,4 @@ from employee import Employee
 # TODO fill all this with tests!
 # README this is a message to read, searchable through the TODOTREE extension
 # FIXME this is a fixme locatable tag
+# TODO another example
